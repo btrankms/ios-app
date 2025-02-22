926efbd3694e70ed8d08f2d7b0242bb85ae92bfa
btrankms <bangtran@kms-technology.com>
2025-02-22 17:08:25 +0700

    fix build

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "ios-app",
+    platforms: [
+        .iOS(.v13)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/danielgindi/Charts.git", .upToNextMajor(from: "4.1.0"))
+    ],
+    targets: [
+        .target(
+            name: "ios-app",
+            dependencies: ["Charts"])
+    ]
+)
